a82abe0fdd5ce40f7a1a59a7d2429c1ad6c06d58
scottman625 <scottman608@gmail.com>
2022-12-10 19:19:38 +0800

    fix error

--- app/stockCrawler/tasks/tasks_seed.py
+++ app/stockCrawler/tasks/tasks_seed.py
@@ -44,10 +44,11 @@ def import_stocks():
             print(stock.name + " " + stock.stock_code)
 
 def import_stock_records():
+    user = User.objects.get(id=2)
     sj.__version__ # 版本為 "0.3.6.dev3"
     api = sj.Shioaji(simulation=False) # simulation=False 即表示使用正式環境
-    PERSON_ID = "D122776936" #身分證字號
-    PASSWORD = "Scott0815" #密碼
+    PERSON_ID = user.PERSON_ID #身分證字號
+    PASSWORD = user.account_password #密碼
     api.login(PERSON_ID, PASSWORD) # 登入
 
     start_date = '2022-01-01'
@@ -89,10 +90,11 @@ def import_stock_records():
     api.logout() # 登出
 
 def import_stock_records_MA():
+    user = User.objects.get(id=2)
     sj.__version__ # 版本為 "0.3.6.dev3"
     api = sj.Shioaji(simulation=False) # simulation=False 即表示使用正式環境
-    PERSON_ID = "D122776936" #身分證字號
-    PASSWORD = "Scott0815" #密碼
+    PERSON_ID = user.PERSON_ID #身分證字號
+    PASSWORD = user.account_password #密碼
     api.login(PERSON_ID, PASSWORD) # 登入
 
     start_date = '2022-01-01'
