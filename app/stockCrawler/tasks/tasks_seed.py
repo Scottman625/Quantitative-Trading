47e79ff996265495f9c984bba06978cbe7a1c4a8
ScottFang <scottman608@gmail.com>
2023-04-03 07:43:55 +0800

    update import stock records

--- app/stockCrawler/tasks/tasks_seed.py
+++ app/stockCrawler/tasks/tasks_seed.py
@@ -81,86 +81,41 @@ def import_stock_records():
             start_date, "%Y-%m-%d").date() - timedelta(days=1)
 
     stocks = Stock.objects.all()
+    print(stock.stock_code)
     for stock in stocks:
-        print(stock.stock_code)
-        if StockRecord.objects.filter(stock=stock, date=datetime.strptime('2022-06-30', '%Y-%m-%d').date()).count() != 0:
-            pass
-        else:
-            try:
-                kbars = api.kbars(
-                    contract=api.Contracts.Stocks[stock.stock_code], start=start_date, end=end_date)
-                df = pd.DataFrame({**kbars})
-                df.ts = pd.to_datetime(df.ts)
-                # df
-                # 分K轉成日K
-                df.set_index(df.ts, inplace=True)
-                df = df.resample('D').agg(
-                    {'Open': 'first', 'High': 'max', 'Low': 'min', 'Close': 'last', 'Volume': 'sum'})
-                df.dropna(inplace=True)
-                for index, row in df.iterrows():
-                    try:
-                        if StockRecord.objects.filter(stock=stock, date=row.name.date()).count() == 0:
-                            stockRecord = StockRecord()
-                        else:
-                            stockRecord = StockRecord.objects.filter(
-                                stock=stock, date=row.name.date()).first()
+        try:
+            kbars = api.kbars(
+                contract=api.Contracts.Stocks[stock.stock_code], start=start_date)
+            df = pd.DataFrame({**kbars})
+            df.ts = pd.to_datetime(df.ts)
+            # df
+            # 分K轉成日K
+            df.set_index(df.ts, inplace=True)
+            df = df.resample('D').agg(
+                {'Open': 'first', 'High': 'max', 'Low': 'min', 'Close': 'last', 'Volume': 'sum'})
+            df.dropna(inplace=True)
+            for index, row in df.iterrows():
+                try:
+                    if StockRecord.objects.filter(stock=stock, date=row.name.date()).count() == 0:
+                        stockRecord = StockRecord()
+                    # else:
+                    #     stockRecord = StockRecord.objects.filter(
+                    #         stock=stock, date=row.name.date()).first()
                         stockRecord.stock = stock
                         stockRecord.date = row.name.date()
-                        stockRecord.OpeningPrice = round(Decimal(row.Open), 2)
-                        stockRecord.ClosingPrice = round(Decimal(row.Close), 2)
+                        stockRecord.OpeningPrice = round(
+                            Decimal(row.Open), 2)
+                        stockRecord.ClosingPrice = round(
+                            Decimal(row.Close), 2)
                         stockRecord.DayHigh = round(Decimal(row.High), 2)
                         stockRecord.DayLow = round(Decimal(row.Low), 2)
                         stockRecord.Volume = round(Decimal(row.Volume), 2)
                         stockRecord.save()
-                    except:
-                        pass
-                # EMA_start_date = datetime.strptime(start_date,"%Y-%m-%d").date() - timedelta(days=1)
-                # stockRecords = StockRecord.objects.filter(stock=stock,date__lte=end_date).order_by('-date')
-                # for stockRecord in stockRecords:
-                #     if stockRecords.filter(date__lte=stockRecord.date).count() == 12:
-                #         stockRecords_12 = stockRecord
-                #         if stockRecords.filter(date=stockRecords_12.date,EMA_12__isnull=False).count() == 0:
-                #             stockRecords_12.EMA_12 = stockRecords_12.MA_12
-                #             stockRecords_12.save()
-                #     if stockRecords.filter(date__lte=stockRecord.date).count() == 26:
-                #         stockRecords_26 = stockRecord
-                #         if stockRecords.filter(date=stockRecords_26.date,EMA_26__isnull=False).count() == 0:
-                #             stockRecords_26.EMA_26 = stockRecords_26.MA_26
-                #             stockRecords_26.save()
-                # stockRecords_12_list = list(stockRecords.filter(date__gte=EMA_start_date).order_by('date'))
-                # for i in range(1,len(stockRecords_12_list)):
-                #     if stockRecords.filter(date=stockRecords_12_list[i].date,EMA_12__isnull=False).count() == 0:
-                #         # cdp = (stockRecords_12_list[i].DayHigh + stockRecords_12_list[i].DayLow + (stockRecords_12_list[i].ClosingPrice * 2))/4
-                #         # print('cdp:',cdp)
-                #         ema12 = round((stockRecords_12_list[i].ClosingPrice * round(decimal.Decimal(2/13),2)) + (stockRecords_12_list[i-1].EMA_12)*(1-round(decimal.Decimal(2/13),2)),2)
-                #         stockRecords_12_list[i].EMA_12 = ema12
-                #         stockRecords_12_list[i].save()
+                except:
+                    pass
 
-                # stockRecords_26_list = list(stockRecords.filter(date__gte=EMA_start_date).order_by('date'))
-
-                # for i in range(1,len(stockRecords_26_list)):
-                #     if (stockRecords.filter(date=stockRecords_26_list[i].date,EMA_26__isnull=False).count() == 0) | (stockRecords.filter(date=stockRecords_26_list[i].date,DIF__isnull=False).count() == 0):
-                #         stockRecords_26_list[i].EMA_26 = round(decimal.Decimal(stockRecords_26_list[i].ClosingPrice * round(decimal.Decimal(2/27),2)) + (stockRecords_26_list[i-1].EMA_26)*(1-round(decimal.Decimal(2/27),2)),2)
-                #         stockRecords_26_list[i].DIF = stockRecords_26_list[i].EMA_12 - stockRecords_26_list[i].EMA_26
-                #         stockRecords_26_list[i].save()
-
-                # for stockRecord in stockRecords:
-                #     if stockRecords.filter(date__lte=stockRecord.date,DIF__isnull=False).count() == 9:
-                #         stockRecords_MACD_9 = stockRecord
-                #         # if stockRecords.filter(date=stockRecords_MACD_9.date,MACD__isnull=False).count() == 0:
-                #         MACD_9 = stockRecords.filter(DIF__isnull=False).order_by('date')[:9].aggregate(Avg('DIF'))['DIF__avg']
-                #         stockRecords_MACD_9.MACD = MACD_9
-                #         stockRecords_MACD_9.save()
-
-                # stockRecords_MACD_list = list(stockRecords.filter(DIF__isnull=False,date__gte=EMA_start_date).order_by('date'))
-                # for i in range(1,len(stockRecords_MACD_list)):
-                #     if stockRecords.filter(date=stockRecords_MACD_list[i].date,MACD__isnull=False).count() == 0:
-                #         stockRecords_MACD_list[i].MACD = round(decimal.Decimal(stockRecords_MACD_list[i-1].MACD + round(decimal.Decimal(2/10),2)*(stockRecords_MACD_list[i].DIF-stockRecords_MACD_list[i-1].MACD)),2)
-                #         stockRecords_MACD_list[i].save()
-
-            except:
-                # print(stock.name + " " + stock.stock_code  + " no index data.")
-                pass
+        except:
+            pass
 
     api.logout()  # 登出
 
