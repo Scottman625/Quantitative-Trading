991bb5d0739f96291fd4971cabdf929fa0a3ca0c
ScottFang <scottman608@gmail.com>
2023-04-03 08:13:26 +0800

    add celery data

--- app/app/celery.py
+++ app/app/celery.py
@@ -69,7 +69,7 @@ def setup_periodic_tasks(sender, **kwargs):
     # )
 
     sender.add_periodic_task(
-        crontab(hour=0, minute=10),
+        crontab(hour=0, minute=23),
         get_recent_N_font_stock.s('get recent N font type stock'),
     )
 
