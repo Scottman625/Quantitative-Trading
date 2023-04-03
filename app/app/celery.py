8af3994c31c7cc2f2034aaec45e1f6ae88ad83cd
ScottFang <scottman608@gmail.com>
2023-04-03 11:36:41 +0800

    add celery data

--- app/app/celery.py
+++ app/app/celery.py
@@ -69,7 +69,7 @@ def setup_periodic_tasks(sender, **kwargs):
     # )
 
     sender.add_periodic_task(
-        crontab(hour=3, minute=29),
+        crontab(hour=3, minute=40),
         import_stock_records.s('import_stock_records'),
     )
 
